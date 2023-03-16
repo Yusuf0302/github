3d959df03c5564e495ee40f41814568fc244fc08
Yusuf <yusufsunnatullayev7@gmail.com>
2023-03-16 16:25:01 +0500

    finished

---
+++ app.js
@@ -0,0 +1,150 @@
+"use strict";
+/*------------- 5 - Dars "O'zgaruvchilar" 👑 ------------- */
+
+// let number = 10;
+// const borderColor = "red";
+
+// console.log(number);
+// number = 20;
+// console.log(number);
+
+// borderColor = "blue";
+
+// const person = {
+//   name: "Samar",
+// };
+
+// person.name = "Yusuf";
+
+// console.log(person.name);
+
+// Old versions
+
+// var name = "Samar";
+// name = "Yusuf";
+
+// console.log(name);
+
+/*------------- 6 - Dars "Qat'iy rejim" 👑 ------------- */
+
+// "use strict";
+
+/*------------- 7 - Dars "Malumot Turlari" 👑 ------------- */
+
+/* ---------    JAVASCRIPT DTA TYPES ---------*/
+
+/* PRIMITIVE */
+
+/* 1 - Boolean
+   2 - Null 
+   3 - Undefind
+   4 - Number 
+   5 - String
+   6 - Symbol */
+
+/* OBJECT */
+
+/* 1 - Array
+   2 - Object
+   3 - Function
+   4 - RegEx 
+   5 - Date */
+
+/*  NUMBER */
+
+// let number = 5.6; // Data Type = Number
+// console.log(-8 / 0); // Infinity
+// console.log("Samar" * 8); // NaN
+
+// /* String */
+// const clientName = `Samar`;
+
+// /* Boolean */
+// const isMarried = false;
+// const isCircleEarth = true;
+
+// /* Null */
+// console.log(clientAge);
+
+// /* Undefind */
+// let und;
+// console.log(und);
+
+// /* Object */
+// const theif = {
+//   //Key: value
+//   age: 30,
+//   Jacket: "black",
+//   isLong: false,
+// };
+
+// // 1-way
+// console.log(theif);
+// console.log(theif.Jacket);
+
+// // 2-way
+// console.log(theif["age"]);
+
+// /* Array */
+// //                0       1         2     3   4   5
+// const colors = ["red", "black", "green", 10, [], {}];
+
+// console.log(colors[0]);
+
+/*------------- 8 - Dars "Foydalanuvchi bilan oddiy muloqot" 👑 ------------- */
+
+// alert("Hello JavaScript");
+// const place = confirm("Are you here?");
+// const currentPlace = prompt("Where are you ?", "");
+// console.log(typeof currentPlace);
+// const age = +prompt("How old are you?", "18");
+// console.log(age + 10);
+// const favouriteColor = [];
+// favouriteColor[0] = prompt("What's your favourite color #1, "");
+// favouriteColor[1] = prompt("What's your favourite color #2, "");
+// favouriteColor[2] = prompt("What's your favourite color #3, "");
+// console.log(favouriteColor);
+
+/*------------- 9 - Dars " Interpolatsiya" 👑 ------------- */
+
+// const channel = prompt("What's you favourite youtuber");
+// console.log(`https://youtube.com/${channel}`)
+// const user = "Samar";
+// const job = "Java Script";
+// console.log(`User name is ${user}, heis @{job}`);
+// console.log("https://youtube.com/" + channel + "/videos");
+// const client = prompt("hello");
+// console.log(client);
+
+/*------------- 10 - Dars " Operatorlar" 👑 ------------- */
+
+// console.log("Samar" + "Badriddinov");
+
+// incr, decr
+
+// let incr = 10;
+// let decr = 10;
+
+// console.log(incr++);
+// console.log(decr--);
+// console.log(incr++);
+// console.log(decr--);
+
+// console.log(incr);
+// console.log(decr);
+
+// console.log(10 % 3);
+
+// console.log(5 * 5 == "25");
+
+// & = va  || = yoki
+
+// const isAge = true;
+// const isClose = false;
+// const isChecked = false;
+
+// console.log(isAge && isClose && !isChecked);
+
+// console.log(2 + 2 * 2 != 8);
+
+/*------------- 11 - Dars "Github bilan ishlash" 👑 ------------- */
